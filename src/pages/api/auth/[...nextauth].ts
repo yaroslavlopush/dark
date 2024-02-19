2c503ce26a6ac5e0efb85d32a4a579b96db39777
Lapuh1st <slavik.lopush@gmail.com>
2024-02-19 22:49:22 +0200

    working example 444

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,11 +5,12 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,   // process.env.GOOGLE_CLIENT_ID
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string                   //  process.env.GOOGLE_CLIENT_SECRET
+      clientId: '419927363606-4j6tg3ris40oljhe7a63g3afq0849ve6.apps.googleusercontent.com',   // process.env.GOOGLE_CLIENT_ID
+      clientSecret: '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'                    //  process.env.GOOGLE_CLIENT_SECRET
     })
     // ...add more providers here
   ],
+  secret: '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'
 }
 
 export default NextAuth(authOptions);
